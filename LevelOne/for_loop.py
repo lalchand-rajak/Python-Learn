5bdb5bfc0cc84fae3d2162754d94c0939a24b6b3
lalchandrajak05 <lalchandrajak05@gmail.com>
2021-02-27 18:01:50 +0530

    LevelOne

---
+++ LevelOne/for_loop.py
@@ -0,0 +1,21 @@
+# print(range(0,5)
+
+# print(list(range(0,20,2)))
+
+mylist = list(range(0,20))
+
+for i in mylist:
+    print(i, end=" ")
+
+for j in "lalchand":
+    print(j)
+
+football = ["torres","werner","kai","mount"]
+
+for f in football:
+    print(f)
+
+mytuple = ( 1, 4, 5, 9, "lalchand")
+
+for t in mytuple:
+    print(t)
---
+++ LevelOne/while_loop.py
@@ -0,0 +1,16 @@
+score = 0
+
+while (score <= 20):
+    print("your score is: ", score)
+    score +=1
+    # score = score + 1
+print("Loop ends here")
+
+
+# user = 1
+#
+# while user == 1:
+#     user_user = int(input("enter the user score: "))
+#     print("Enter the value of user",user_user)
+#
+# print("loop ends")
