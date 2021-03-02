4e14603b9124bca4c8275c1632346fb5743dff35
lalchandrajak05 <lalchandrajak05@gmail.com>
2021-03-02 16:27:05 +0530

    LevelTwo

---
+++ LevelTwo/py_math.py
@@ -0,0 +1,20 @@
+import math
+print(abs(-45))
+print(abs(45))
+
+print(math.fabs(-45))
+
+print(math.ceil(-45.88))  #ceil function  chooses nearest highest value
+print(math.ceil(200.12))
+
+
+print(math.floor(-45.88)) #floor function  chooses nearest lowest value
+
+print(math.floor(200.12))
+
+
+print(max(10,20,40,99,100,205))
+print(min(10,20,40,99,100,205))
+
+print(math.sqrt(9))
+print(math.sqrt(4))
